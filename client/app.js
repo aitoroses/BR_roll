a921ccecffeacc0334eae3ff29b3fccdc13014e6
Aitor Oses <aitor.oses@gmail.com>
2014-06-13 15:15:43 +0000

    I need broll-plugins

--- client/app.js
+++ client/app.js
@@ -4,12 +4,16 @@ Meteor.startup(function(){
   "use strict";
 
   var Engine  = require('famous/core/Engine');
+  var MiniMenu = require('broll/MiniMenu/views/AppView');
+  var Router = require('Router');
 
   var mainCtx = Engine.createContext();
 
-  var Router = require('Router');
-
   var router = new Router();
+
+  var menu = new MiniMenu();
+
+  router.add(menu);
   
   mainCtx.add(router.view());
   
